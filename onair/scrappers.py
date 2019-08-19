8dd7c33eb584c7246a45169e32e2d2dc6e1dbe87
Yegor Ivashchenko <yegor.ivashchenko@protonmail.com>
2019-08-19 23:23:30 +0300

    add more radio stations

--- onair/scrappers.py
+++ onair/scrappers.py
@@ -17,31 +17,26 @@ def i_ua(link):
     except:
         log.exception("Unexpected error while scapping i.ua {}:".format(link))
 
-
-def hit_fm():
-    return {'station': 'Хiт FM', 'onair': i_ua('hit.fm')}
-
-
-def russkoe_radio_ukraina():
-    return {'station': '"Русское Радио" Украина', 'onair': i_ua('russkoe.radio')}
-
-def nrj():
-    return {'station': 'NRJ', 'onair': i_ua('nrj')}
-
-def kiss_fm():
-    return {'station': 'Kiss FM', 'onair': i_ua('kiss.fm')}
-
-def melodiya():
-    return {'station': 'Мелодия FM', 'onair': i_ua('melodiya')}
-
-def nashe_radio():
-    return {'station': 'Наше Радио', 'onair': i_ua('nashe.radio')}
-
-def uafm():
-    return {'station': 'UAFM', 'onair': i_ua('uafm.km.ua')}
-
-def keksfm():
-    return {'station': 'KEXXX FM', 'onair': i_ua('keksfm')}
-
-def radiopyatnica():
-    return {'station': 'Радио ПЯТНИЦА', 'onair': i_ua('radiopyatnica')}
+I_UA = [
+    lambda: {'station': '"Русское Радио" Украина', 'onair': i_ua('russkoe.radio')},
+    lambda: {'station': 'NRJ', 'onair': i_ua('nrj')},
+    lambda: {'station': 'Kiss FM', 'onair': i_ua('kiss.fm')},
+    lambda: {'station': 'KEXXX FM', 'onair': i_ua('keksfm')},
+    lambda: {'station': '109FM', 'onair': i_ua('109fmnet')},
+    lambda: {'station': 'BiG Radio', 'onair': i_ua('bigradio.com.ua')},
+    lambda: {'station': 'Metal Voice Радио', 'onair': i_ua('metalvoice')},
+    lambda: {'station': 'MiX FM', 'onair': i_ua('vtsu.dance')},
+    lambda: {'station': 'PsyRadio', 'onair': i_ua('psyradio')},
+    lambda: {'station': 'RadioEx', 'onair': i_ua('radioex')},
+    lambda: {'station': 'Rock FM Ukraine', 'onair': i_ua('prorock.online')},
+    lambda: {'station': 'TRANCE IS STAR', 'onair': i_ua('tranceisstar')},
+    lambda: {'station': 'UAFM', 'onair': i_ua('uafm.km.ua')},
+    lambda: {'station': 'Мелодия FM', 'onair': i_ua('melodiya')},
+    lambda: {'station': 'Наше Радио', 'onair': i_ua('nashe.radio')},
+    lambda: {'station': 'Радио VTSU', 'onair': i_ua('vtsu.org.ua')},
+    lambda: {'station': 'Радио Вечерний Бриз', 'onair': i_ua('rpr.dp.ua')},
+    lambda: {'station': 'Радио ПЯТНИЦА', 'onair': i_ua('radiopyatnica')},
+    lambda: {'station': 'Радіо з криївки', 'onair': i_ua('kriyivki')},
+    lambda: {'station': 'РокРадио UA', 'onair': i_ua('rockradio')},
+    lambda: {'station': 'Хiт FM', 'onair': i_ua('hit.fm')},
+]
