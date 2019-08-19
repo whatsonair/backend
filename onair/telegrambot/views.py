5591b94b7c62e0de13ad63c7c7b3effea06d917d
Yegor Ivashchenko <yegor.ivashchenko@protonmail.com>
2019-08-19 15:06:03 +0300

    add csrf_exempt to webhook view

--- onair/telegrambot/views.py
+++ onair/telegrambot/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def telegram_webhook(request):
     return HttpResponse()
