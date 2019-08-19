e8916ad4b46b3e862b6ed33aec66bb0945d228a0
Yegor Ivashchenko <yegor.ivashchenko@protonmail.com>
2019-08-19 14:46:54 +0300

    add telegram webhook placeholder

--- onair/telegrambot/urls.py
+++ onair/telegrambot/urls.py
@@ -1,7 +1,4 @@
-"""onair URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+"""
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +10,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from onair.telegrambot.views import telegram_webhook
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('telegram/', include('telegrambot.urls'))
+    path('webhook/', telegram_webhook),
 ]
